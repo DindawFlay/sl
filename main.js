c8e0b7e7d5f3b22f64eaca7dd934d9115bb1788a
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-03-17 11:42:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ nocache('./index.js', module => console.log(`${module} is now updated!`))
 
 const starts = async (hexa = new WAConnection()) => {
     hexa.logger.level = 'warn'
-    hexa.version = [2, 2119, 6]
+    hexa.version = [2, 2140, 12]
     hexa.browserDescription = [ 'Hexagonz', 'Chrome', '3.0' ]
     console.log(banner.string)
     hexa.on('qr', () => {
